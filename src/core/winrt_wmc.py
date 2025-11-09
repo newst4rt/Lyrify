02ab59e61f166f6c83d8fe0973b5d0193e351c04
newst4rt <105312719+newst4rt@users.noreply.github.com>
2025-11-09 20:29:16 +0100

    fix: error if player get closed

--- src/core/winrt_wmc.py
+++ src/core/winrt_wmc.py
@@ -6,11 +6,14 @@ import time
 class Wmc():
 
     def __init__(self, session):
-
-        session.add_timeline_properties_changed(self.on_timeline)
-        session.add_media_properties_changed(self.on_media)
-        session.add_playback_info_changed(self.on_playback)
-
+        if not isinstance(session, bool):
+            if not self.in_event_loop():
+                self.on_media(session, None)
+            self.on_playback(session, None)
+            self.on_timeline(session, "init")
+            session.add_timeline_properties_changed(self.on_timeline)
+            session.add_media_properties_changed(self.on_media)
+            session.add_playback_info_changed(self.on_playback)
 
     def on_media(self, sender, args):
         asyncio.run(self.get_media_props(sender))
@@ -24,40 +27,76 @@ class Wmc():
         pb_info = sender.get_playback_info()
         if pb_info.playback_status.name == "PLAYING":
             self.pb_state = True
+            timeline = sender.get_timeline_properties()
+            self.timesync = time.perf_counter()
+            self.position = float(timeline.position.total_seconds()*1000)
         else:
             self.pb_state = False
+    def in_event_loop(self):
+        try:
+            asyncio.get_running_loop()
+            return True
+        except RuntimeError:
+            return False
 
     @classmethod
     async def create(cls, player):
-        manager = await GlobalSystemMediaTransportControlsSessionManager.request_async()
-        sessions = manager.get_sessions()
-        session = next((x for x in sessions if player in x.source_app_user_model_id), None)
+        cls.player = player
+        cls.manager = await GlobalSystemMediaTransportControlsSessionManager.request_async()
+        session = cls.get_session(cls, player, cls.manager)
         if session:
+            cls.status = True
+            cls.session = session
             await cls.get_media_props(cls, session)
-            cls.on_playback(cls, session, None)
-            cls.on_timeline(cls, session, "init")
             return cls(session)
         else:
-            raise Exception(f"There is no player running with the name {player}.") 
+            cls.status = False
+            return cls(cls.status)
+    
+    def get_session(self, player, manager):
+        sessions = manager.get_sessions()
+        session = next((x for x in sessions if player in x.source_app_user_model_id), None)
+        return session
 
     async def get_media_props(self, session):
-        player_metadata = await session.try_get_media_properties_async()
-        self.artist = player_metadata.artist
-        self.title = player_metadata.title
-        _timeline = session.get_timeline_properties()
-        self.track_len = _timeline.end_time.total_seconds()*1000
-        self.new_track = True
+        try:
+            player_metadata = await session.try_get_media_properties_async()
+            self.artist = player_metadata.artist
+            self.title = player_metadata.title
+            _timeline = session.get_timeline_properties()
+            self.track_len = _timeline.end_time.total_seconds()*1000
+            self.new_track = True
+        except Exception:
+            pass
         
     def get_track_data(self, past_id: bool | None):
-            delta_timesync = float(self.position+((time.perf_counter()-self.timesync)*1000)) 
-            if delta_timesync > self.track_len:
-                delta_timesync = self.track_len 
-            if self.new_track:
-                self.new_track = False
-                past_id = True if past_id == False else False
-                if self.track_len == self.position:
-                    return 3
-                return past_id, float(delta_timesync), float(self.track_len), str(self.artist.replace(" ", "+")), str(self.title.replace(" ", "+"))
-            
+
+        try:
+            pb_info = self.session.get_playback_info()
+            if pb_info.controls.is_play_enabled == False and pb_info.controls.is_pause_enabled == False: # Player is not running
+                raise()
+        except Exception:
+            self.session = self.get_session(self.player, self.manager)
+            if self.session:
+                self.__init__(self.session)
+                pb_info = self.session.get_playback_info()
             else:
-                return past_id, float(delta_timesync)
+                return 2
+                
+        if pb_info.playback_status.name == "PLAYING":
+            self.delta_timesync = float(self.position+((time.perf_counter()-self.timesync)*1000)) 
+        else:
+            self.delta_timesync = self.position
+
+        if self.delta_timesync > self.track_len:
+            self.delta_timesync = self.track_len 
+
+        if self.new_track:
+            past_id = True if past_id == False else False
+            self.new_track = False
+            if self.track_len == self.position:
+                return 3
+            return past_id, float(self.delta_timesync), float(self.track_len), str(self.artist.replace(" ", "+")), str(self.title.replace(" ", "+"))
+        
+        else:
+            return past_id, float(self.delta_timesync)
